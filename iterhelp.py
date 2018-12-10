530a70fe0f7aefac20079d5a740a8cf4e219ebe0
Sandbom <simon.sandbom@gmail.com>
2018-12-10 23:40:58 +0100

    Adding tests for 7 itertools functions, more info in itertest

---
+++ iterhelp.py
@@ -0,0 +1,86 @@
+from itertools import cycle
+from itertools import permutations
+from itertools import chain
+from itertools import filterfalse
+
+def cyclestop(input, cycle_for):
+	#Stops cycle after n=cycle_for elements and puts items iterated through
+	# in list
+	cycle_items = []
+	for i, item in enumerate(cycle(input)):
+		if i < cycle_for:
+			cycle_items.append(item)
+		else:
+			break
+
+	return cycle_items
+
+def permhelp(to_permutate):
+	#Puts each permutation as an element in a list
+	result = permutations(to_permutate)
+	list_permutations = []
+	for each in result:
+		list_permutations.append(list(each))
+
+	return list_permutations
+
+
+def chainhelp(*iterables):
+	#Puts all results generated from chain in list
+	all_iterable_elements = []
+	for item in chain(*iterables):
+		all_iterable_elements.append(item)
+
+	return all_iterable_elements
+
+def chainhelp_single(iterables):
+	all_iterable_elements_fi = []
+	for item in chain.from_iterable(iterables):
+		all_iterable_elements_fi.append(item)
+
+	return all_iterable_elements_fi
+
+#Returns items filtered through filterfalse in list
+def filterfalsehelp(predicate, iterable):
+	filtered_elements = []
+
+	for item in filterfalse(predicate, iterable):
+		filtered_elements.append(item)
+
+	return filtered_elements
+
+#Predicate function used in filterfalse to filter out list iterable
+def filterfalse_pred1(input):
+	iterable = ['b','d','c']
+	if input in iterable:
+		return True
+	else:
+		return False
+
+#Predicate function used in filterfalse to filter out list iterable
+def filterfalse_pred2(input):
+	iterable = ["abcdef","ghi","lmnopjk","123"]
+	if input in iterable:
+		return True
+	else:
+		return False
+
+#Predicate function used in filterfalse to filter out list iterable
+def filterfalse_pred3(input):
+	iterable = [[1,2],['d','e','f'],["test1","test2","test3"]]
+	if input in iterable:
+		return True
+	else:
+		return False
+
+
+
+
+
+
+
+
+
+
+
+
